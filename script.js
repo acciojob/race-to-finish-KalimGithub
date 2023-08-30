235a96a7af276fd6cfaa3c320aa9135d5ac3f75f
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-08-30 20:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 window.promises = [];
-
+let x = promises.any([
+		new promise(resolve,reject) => {
+			setTimeout(() =>{
+				resolve("one resolved");
+			},1000)
+		}
+		new promise(resolve,reject) => {
+			setTimeout(() =>{
+				resolve("one resolved");
+			},2000)
+		}
+		new promise(resolve,reject) => {
+			setTimeout(() =>{
+				resolve("one resolved");
+			},2000)
+		}
+		new promise(resolve,reject) => {
+			setTimeout(() =>{
+				resolve("one resolved");
+			},4000)
+		}
+		new promise(resolve,reject) => {
+			setTimeout(() =>{
+				resolve("one resolved");
+			},3000)
+		}
+	])
+const myDiv = document.getElementById("output");
+x.then((data){
+myDiv.innerHTML = data;
+})
 // Do not change the code above this
 // add your promises to the array `promises`
